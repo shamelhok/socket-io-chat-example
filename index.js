d8c62dc9bc2a0ce9ccee8fcbc620a56270ee0ddd
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-31 15:16:53 +0100

    chage port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -18,7 +19,6 @@ io.on('connection', (socket) => {
   io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
-module.exports=app
