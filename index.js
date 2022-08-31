c5cb0230c442defe3b8164d3f97742eff40e4621
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-31 12:55:40 +0100

    followed the rest of the get started page

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
 });
 
 server.listen(4001, () => {
