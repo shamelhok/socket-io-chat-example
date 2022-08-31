6a66ea8abcba3d438c29a01d889f751c73ae932d
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-31 15:48:06 +0100

    add number of sockets

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.emit('chat message','userconnected')
+  socket.emit('chat message',`user connected, currently online${
+    io.sockets.sockets.length}`)
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
     socket.emit('chat message', msg);
